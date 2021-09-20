d075dd7e7ee69f1e27e9ce49bc60f969b03be909
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 00:51:32 -0300

    dados classe pessoa

--- src/br/com/zup/Main.java
+++ src/br/com/zup/Main.java
@@ -1,5 +1,7 @@
 package br.com.zup;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -18,8 +20,18 @@ public class Main {
         System.out.println("Marca: " + objeto2.marca);
         System.out.println("Velocidade: " + objeto2.velocidade);
 
-        Pessoa pessoa1 = new Pessoa("Maria", "38", "70", "1.70");
+        Pessoa pessoa1 = new Pessoa("Maria João Silva Sauro", "38", "70", "1.70");
         System.out.println("___Classe Pessoa___");
         System.out.println("Nome: " +pessoa1.nome);
+        System.out.println("Idade: " +pessoa1.idade+ " anos");
+        System.out.println("Peso: " +pessoa1.peso+ " kg");
+        System.out.println("Altura: " +pessoa1.altura+ " m");
+
+        Pessoa pessoa2 = new Pessoa("Joana Maria", "46", "65", "1.56");
+        System.out.println("___Classe Pessoa___");
+        System.out.println("Nome: " +pessoa2.nome);
+        System.out.println("Idade: " +pessoa2.idade+ " anos");
+        System.out.println("Peso: " +pessoa2.peso+ " kg");
+        System.out.println("Altura: " +pessoa2.altura+ " m");
     }
 }
