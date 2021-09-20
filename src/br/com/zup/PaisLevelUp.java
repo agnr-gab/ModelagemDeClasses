311f6bd9b54aec2647771e32ffcd490b0ad821a3
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 00:32:34 -0300

    level up

---
+++ src/br/com/zup/PaisLevelUp.java
@@ -0,0 +1,11 @@
+package br.com.zup;
+//1. Classe País: Crie uma classe que modele um país, contendo nome, extensão territorial e tamanho da população. No método main, crie uma lista chamada continente, onde teremos uma lista de países. Adicione 4 países na lista do continente (Países de sua escolha) e crie um menu para o usuário, que deverá decidir se vai listar os países do continente ou sair do programa.
+public class PaisLevelUp {
+    String nomePais;
+    double extensaoTerritorial;
+    double tamanhoPop;
+
+    public PaisLevelUp (String nomePais, double extensaoTerritorial, double tamanhoPop) {
+
+    }
+}
