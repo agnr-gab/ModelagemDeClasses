e74b980d51c6a3545e88265f9e27aa7b9623be86
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 00:04:25 -0300

    finalizado classe pessoa

--- src/br/com/zup/Pessoa.java
+++ src/br/com/zup/Pessoa.java
@@ -1,4 +1,16 @@
 package br.com.zup;
 //2. Classe Pessoa: Crie uma classe que modele uma pessoa, toda pessoa contém nome, idade, peso e altura. No construtor, o peso é opcional, com valor default (padrão) zero e os demais atributos são obrigatórios.
 public class Pessoa {
+    String nome;
+    int idade;
+    double peso;
+    double altura;
+
+    public Pessoa (String nome, int idade, double peso, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.peso = peso;
+        this.altura = altura;
+
+    }
 }
