2cfb368895c5df77a5db16009b9b9598e39a3047
Arê Nascimento <aretuza.gab@gmail.com>
2021-09-20 00:18:56 -0300

    Classe Conta Corrente

---
+++ src/br/com/zup/ContaCorrente.java
@@ -0,0 +1,15 @@
+package br.com.zup;
+
+// 3. Classe Conta Corrente: Crie uma classe para construir uma conta corrente. A classe deve possuir os seguintes atributos: número da conta, nome do correntista e saldo. No construtor, o saldo é opcional, com valor default (padrão) zero e os demais atributos são obrigatórios.
+public class ContaCorrente {
+    String numeroConta;
+    String nomeCorrentista;
+    double saldo;
+
+    public ContaCorrente(String numeroConta, String nomeCorrentista, double saldo) {
+        this.numeroConta = numeroConta;
+        this.nomeCorrentista = nomeCorrentista;
+        this.saldo = 0;
+
+    }
+}
